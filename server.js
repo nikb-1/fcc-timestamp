a15701d790072d77c953f11e501cd3d948ae71c1
Nikola Burazor <nikola.burazor@gmail.com>
2016-09-19 11:28:55 +0000

    remove hello world, add get param

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:timein', function (req, res) {
+	console.log(req.params.timein);
+	res.send(req.params.timein);
 });
 
 app.listen(8080, function () {
