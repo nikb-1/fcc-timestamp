b51c0d82ac02414b2677cafe5338d0ba9496003f
Nikola Burazor <nikola.burazor@gmail.com>
2016-09-19 12:54:01 +0000

    API working as expected

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
 app.get('/:timein', function (req, res) {
-	console.log(req.params.timein);
-	res.send(req.params.timein);
+	jsonRes = {"unix":null,"natural":null};
+	var timeParse = new Date(req.params.timein);
+
+	if (!(isNaN(timeParse) || timeParse == 'Invalid Date')){
+		jsonRes.unix=timeParse.getTime();
+		jsonRes.natural=months[timeParse.getMonth()]+' '+timeParse.getDate()+', '+timeParse.getFullYear();
+	}
+	else if (!(isNaN(Number(String(req.params.timein))))){
+		timeParse = new Date(parseInt(req.params.timein));
+		if (!(timeParse == 'Invalid Date')){
+			jsonRes.unix=timeParse.getTime();
+			jsonRes.natural=months[timeParse.getMonth()]+' '+timeParse.getDate()+', '+timeParse.getFullYear();
+		}
+	}
+	res.end(JSON.stringify(jsonRes));	
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Timestamp Microservice listening on port 8080!');
 });
 
